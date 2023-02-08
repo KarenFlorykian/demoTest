1cd71091e7ff3fdf9c5b1ce5c81c8daf80d4ad3b
KarenFlorykian <karefkaren33@gmail.com>
2023-02-08 17:02:37 +0200

    added html report creation for k6

--- k6-boilerplate/src/tests/stg-todos.js
+++ k6-boilerplate/src/tests/stg-todos.js
@@ -164,6 +164,6 @@ export default function () {
 
 export function handleSummary(data) {
     return {
-      "summary.html": htmlReport(data),
+      "/tmp/summary.html": htmlReport(data),
     };
   }
